41c79d379455507e0d4c708e0cdadd2851c2a883
DelaneyArkICT <delaneywrightbusiness@gmail.com>
2025-04-10 22:50:32 +0100

    fix vite and package config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
- plugins: [vue()],
- build: {
-  lib: {
-   entry: 'index.ts',
-   name: 'LaravelTurnstileVue',
-   fileName: 'laravel-turnstile-vue',
-   formats: ['es', 'umd']
-  },
-  rollupOptions: {
-   external: ['vue'],
-   output: {
-    globals: {
-     vue: 'Vue'
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'index.ts',
+      name: 'LaravelTurnstileVue',
+      fileName: format => `laravel-turnstile-vue.${format}.js`,
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
-   }
   }
- }
 })
